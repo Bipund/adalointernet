197598d0be9e323a1de47ae897cbbf48511c2a21
Bipund <Bipund.pandey@pcodertechnologies.com>
2021-06-03 01:05:10 +0530

    first commit

---
+++ src/components/Internetcheck/index.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+import NetInfo from "@react-native-community/netinfo";
+
+class Internetcheck extends Component {
+
+		constructor(props){
+			super(props);
+			this.state={
+				isActive:true
+			}
+		}
+
+	componentDidMount(){
+		NetInfo.fetch().then(state => {
+			this.setState({isActive:state.isConnected})
+		  });
+	}
+
+
+	render() {
+		const { color, text } = this.props
+
+		return (
+			<View style={styles.wrapper}>
+				{this.state.isActive==true?
+				<Text style={{ color:"green" }}>{"internet is working"}</Text>
+				:
+				<Text style={{ color:"red" }}>{"internet is not working"}</Text>
+			}
+
+			</View>
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+	wrapper: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+	}
+})
+
+export default Internetcheck
